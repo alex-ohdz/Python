60b69dd54705b1d1779a62a5bd5fe5eb835cca34
Alejandro Oramas H <alejandrooramash@gmail.com>
2023-04-28 18:13:26 -0400

    primer commit

---
+++ src/Probando.py
@@ -0,0 +1,20 @@
+# import sys
+# parsecs_input = input(' Input number of parsecs: ')
+# parsecs = int(parsecs_input)
+# lightyears = parsecs * 3.26
+# print (str(parsecs) + ' parsecs is ' + str(lightyears) + ' lightyears')
+
+
+# sys.argv[0]
+##################################
+
+# print("Welcome to the greeter program")
+# name = input("Enter your name: ")
+# print("Greetings " + name)
+
+##################################
+
+# print("calculator program")
+# first_number = input("first number: ")
+# second_number = input("second number: ")
+# print(int(first_number) + int(second_number))
